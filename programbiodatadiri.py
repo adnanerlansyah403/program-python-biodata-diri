dc02b5cdd2b35fd51270b4131a24224d585f4402
Adnan Erlansyah <adnanerlansyah403@gmail.com>
2021-10-27 17:52:22 +0700

    File Program Python Membuat Biodata Diri

---
+++ programbiodatadiri.py
@@ -0,0 +1,11 @@
+namalengkap = input("Nama Lengkap : ");
+namapanggilan = input("Nama Panggilan : ");
+npm = input("NPM : ");
+tempatlahir = input("Tempat lahir : ");
+umur = input("Umur anda : ");
+alamat = input("Alamat anda : ");
+telepon = input("No telepon anda : ");
+
+
+
+print("Assalamualaikum \n\n", "My Name is", namalengkap, ", but you can call me", namapanggilan, "My NPM is", npm, ". I was born in", tempatlahir, "and l am", umur, "I am very glad if you want to invite my house in", alamat, ". So, don't forget to call me before with the number", telepon, ".\n\n Thank you.");
